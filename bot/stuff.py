f9948ea1c5791c62a770b12074ebe7d31b100a92
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-09 15:22:23 +0530

    Update stuff.py

--- bot/stuff.py
+++ bot/stuff.py
@@ -4,10 +4,15 @@ from pyrogram import Client as Bot, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from .worker import *
 
+import os
+from script import Script
+from pyrogram import Client as Bot, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from .worker import *
 
-@Bot.on_message(filters.command(["start"]) & filters.private)
-async def start(bot, update):
-    await update.reply_text(
+async def start(event):
+    ok = await event.client(GetFullUserRequest(event.sender_id))
+    await event.reply(
         text=Script.START_TEXT.format(update.from_user.mention),
         disable_web_page_preview=True,
         reply_markup=Script.START_BUTTONS
@@ -23,3 +28,5 @@ async def up(event):
     ms = (ed - stt).microseconds / 1000
     p = f"🌋Pɪɴɢ = {ms}ms"
     await event.reply(v + "\n" + p)
+
+
