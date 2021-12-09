9f3ccd7628a5cddbb3d9176a8777839397d5b229
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-09 15:01:52 +0530

    Update stuff.py

--- bot/stuff.py
+++ bot/stuff.py
@@ -1,11 +1,11 @@
 import os
 from script import Script
-from pyrogram import Client, filters
+from pyrogram import Client as Bot, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from .worker import *
 
 
-@Client.on_message(filters.command(["start"]) & filters.private)
+@Bot.on_message(filters.command(["start"]) & filters.private)
 async def start(bot, update):
     await update.reply_text(
         text=Script.START_TEXT.format(update.from_user.mention),
